e5abcf38ea614a1b1f0a5068cedaedaac037dd12
ArjunSahlot <iarjun.sahlot@gmail.com>
2024-01-19 23:02:58 -0800

    tried adding animations, mission failed, we'll get'em next time added menu/refactoring/new files/blah/blah

--- src/game.js
+++ src/game.js
@@ -1,12 +1,13 @@
 let moves = 0;
 let currGrid = -1;
-let finishedGrids = [];
+const finishedGrids = [];
+const gameState = {};
 
-let turn = () => {
+const turn = () => {
 	return moves % 2 === 0;
 };
 
-let moveGrid = (grid) => {
+const moveGrid = (grid) => {
 	let newGrid = 0;
 	if (grid === -1) {
 		newGrid = document.getElementsByClassName("big")[0];
@@ -28,7 +29,7 @@ let moveGrid = (grid) => {
 	currGrid = grid;
 };
 
-let updateTurn = (flip = false) => {
+const updateTurn = (flip = false) => {
 	if (flip) {
 		moves++;
 	}
@@ -37,7 +38,7 @@ let updateTurn = (flip = false) => {
 	setCell(info);
 };
 
-let setCell = (cell, index = 0) => {
+const setCell = (cell, index = 0) => {
 	const img = document.createElement("img");
 	img.src = turn() ? "assets/x.svg" : "assets/o.svg";
 	img.classList.add("inline");
@@ -49,29 +50,29 @@ let setCell = (cell, index = 0) => {
 		() => {
 			img.classList.remove("cell-animation");
 		},
-		{ once: true }
+		{ once: true },
 	);
 };
 
-let removeCell = (cell) => {
-	let img = cell.querySelector("img");
+const removeCell = (cell) => {
+	const img = cell.querySelector("img");
 	if (img) {
 		cell.removeChild(img);
 	}
 };
 
-let checkCell = (cell) => {
+const checkCell = (cell) => {
 	// true if x, false if o, null if empty
-	let img = cell.querySelector("img");
+	const img = cell.querySelector("img");
 	if (img) {
 		return img.src.includes("x");
 	}
 	return null;
 };
 
-let getGrid = () => {
+const getGrid = () => {
 	// grid[gridIndex][row][col]
-	let grid = [];
+	const grid = [];
 	for (let i = 0; i < 9; i++) {
 		grid.push([]);
 		grid[i].push([]);
@@ -85,25 +86,43 @@ let getGrid = () => {
 	return grid;
 };
 
-let checkGrid = (grid) => {
+const checkGrid = (grid) => {
 	// check minigrid for winner
 	for (let i = 0; i < 3; i++) {
-		if (grid[i][0] !== null && grid[i][0] === grid[i][1] && grid[i][1] === grid[i][2]) {
+		if (
+			grid[i][0] !== null &&
+			grid[i][0] === grid[i][1] &&
+			grid[i][1] === grid[i][2]
+		) {
 			return grid[i][0];
-		} else if (grid[0][i] !== null && grid[0][i] === grid[1][i] && grid[1][i] === grid[2][i]) {
+		}
+
+		if (
+			grid[0][i] !== null &&
+			grid[0][i] === grid[1][i] &&
+			grid[1][i] === grid[2][i]
+		) {
 			return grid[0][i];
 		}
 	}
-	if (grid[0][0] !== null && grid[0][0] === grid[1][1] && grid[1][1] === grid[2][2]) {
+	if (
+		grid[0][0] !== null &&
+		grid[0][0] === grid[1][1] &&
+		grid[1][1] === grid[2][2]
+	) {
 		return grid[0][0];
 	}
-	if (grid[0][2] !== null && grid[0][2] === grid[1][1] && grid[1][1] === grid[2][0]) {
+	if (
+		grid[0][2] !== null &&
+		grid[0][2] === grid[1][1] &&
+		grid[1][1] === grid[2][0]
+	) {
 		return grid[0][2];
 	}
 	return null;
 };
 
-let updateGrid = (grid, cellIndex) => {
+const updateGrid = (grid, cellIndex) => {
 	if (finishedGrids.includes(cellIndex)) {
 		moveGrid(-1);
 	} else {
@@ -111,11 +130,11 @@ let updateGrid = (grid, cellIndex) => {
 	}
 	for (let i = 0; i < 9; i++) {
 		if (finishedGrids.includes(i)) continue;
-		let winner = checkGrid(grid[i]);
+		const winner = checkGrid(grid[i]);
 		if (winner !== null) {
 			finishedGrids.push(i);
-			let miniGrid = document.getElementsByClassName("small")[i];
-			let overlay = document.createElement("div");
+			const miniGrid = document.getElementsByClassName("small")[i];
+			const overlay = document.createElement("div");
 			overlay.classList.add("overlay");
 			overlay.style.backgroundImage = `url(assets/${winner ? "x" : "o"}.svg)`;
 			miniGrid.parentElement.insertBefore(overlay, miniGrid);
@@ -125,78 +144,81 @@ let updateGrid = (grid, cellIndex) => {
 	}
 };
 
-let clearGrid = () => {
-	document.querySelectorAll(".cell").forEach((cell) => {
+const clearGrid = () => {
+	const cells = document.querySelectorAll(".cell");
+
+	for (const cell of cells) {
 		removeCell(cell);
-	});
+	}
 };
 
-document.addEventListener("DOMContentLoaded", function () {
-	const miniGrids = document.querySelectorAll(".small");
-	const cells = document.querySelectorAll(".cell");
-	const start = document.getElementById("start");
-	const info = document.getElementById("info");
-	const grid = document.getElementsByClassName("big")[0];
+const miniGrids = document.querySelectorAll(".small");
+const cells = document.querySelectorAll(".cell");
+const start = document.getElementById("start");
+const info = document.getElementById("info");
+const grid = document.getElementsByClassName("big")[0];
 
-	start.addEventListener("click", () => {
-		clearGrid();
-		moves = -1;
-		updateTurn(true);
-		info.classList.remove("hidden");
+start.addEventListener("click", () => {
+	clearGrid();
+	moves = -1;
+	updateTurn(true);
+	info.classList.remove("hidden");
+	grid.classList.remove("hidden");
+	moveGrid(-1);
+	for (const grid of miniGrids) {
 		grid.classList.remove("hidden");
-		moveGrid(-1);
-		miniGrids.forEach((grid) => {
-			grid.classList.remove("hidden");
-		});
-		document.querySelectorAll(".overlay").forEach((overlay) => {
-			overlay.remove();
-		});
-		document.getElementById("start").innerHTML = "Reset Game";
-	});
-
-	miniGrids.forEach((grid) => {
-		grid.addEventListener("mouseover", () => {
-			if (moves !== 0) return;
-			const allGrids = Array.from(document.querySelectorAll(".small"));
-			const gridIndex = allGrids.indexOf(grid);
-			moveGrid(gridIndex);
-		});
-	});
-
-	cells.forEach((cell) => {
-		cell.addEventListener("mouseover", () => {
-			cell.classList.add("cell-highlight");
-		});
-
-		cell.addEventListener("mouseout", () => {
-			cell.classList.remove("cell-highlight");
-		});
-
-		cell.addEventListener("click", () => {
-			const parentGrid = cell.parentElement;
-			const allGrids = Array.from(document.querySelectorAll(".small"));
-			const allCellsInGrid = Array.from(parentGrid.children);
-
-			const gridIndex = allGrids.indexOf(parentGrid);
-			const cellIndex = allCellsInGrid.indexOf(cell);
-
-			const border = document.querySelector(".border");
-
-			if (checkCell(cell) === null && (currGrid === -1 || gridIndex === currGrid)) {
-				setCell(cell);
-				updateTurn(true);
-				updateGrid(getGrid(), cellIndex);
-			} else {
-				border.classList.add("shake");
-
-				border.addEventListener(
-					"animationend",
-					() => {
-						border.classList.remove("shake");
-					},
-					{ once: true }
-				);
-			}
-		});
-	});
+	}
+	for (const overlay of document.querySelectorAll(".overlay")) {
+		overlay.remove();
+	}
+	document.getElementById("start").innerHTML = "Reset Game";
 });
+
+for (const grid of miniGrids) {
+	grid.addEventListener("mouseover", () => {
+		if (moves !== 0) return;
+		const allGrids = Array.from(document.querySelectorAll(".small"));
+		const gridIndex = allGrids.indexOf(grid);
+		moveGrid(gridIndex);
+	});
+}
+
+for (const cell of cells) {
+	cell.addEventListener("mouseover", () => {
+		cell.classList.add("cell-highlight");
+	});
+
+	cell.addEventListener("mouseout", () => {
+		cell.classList.remove("cell-highlight");
+	});
+
+	cell.addEventListener("click", () => {
+		const parentGrid = cell.parentElement;
+		const allGrids = Array.from(document.querySelectorAll(".small"));
+		const allCellsInGrid = Array.from(parentGrid.children);
+
+		const gridIndex = allGrids.indexOf(parentGrid);
+		const cellIndex = allCellsInGrid.indexOf(cell);
+
+		const border = document.querySelector(".border");
+
+		if (
+			checkCell(cell) === null &&
+			(currGrid === -1 || gridIndex === currGrid)
+		) {
+			setCell(cell);
+			updateTurn(true);
+			updateGrid(getGrid(), cellIndex);
+		} else {
+			border.classList.add("shake");
+
+			border.addEventListener(
+				"animationend",
+				() => {
+					border.classList.remove("shake");
+				},
+				{ once: true },
+			);
+		}
+	});
+}
