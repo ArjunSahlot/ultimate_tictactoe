e8b461648071aef43d571fb14267ecdc88a7d0ee
ArjunSahlot <iarjun.sahlot@gmail.com>
2024-03-17 13:09:45 -0700

    global everything

---
+++ src/js/ai.js
@@ -0,0 +1,32 @@
+import { Board } from "./board.js";
+
+const board = new Board();
+
+document.getElementById("start").addEventListener("click", () => {
+  board.reset();
+});
+
+for (const grid of board.boardUI.small) {
+  grid.addEventListener("mouseover", () => {
+    if (board.moves !== 0) return;
+    board.boardUI.highlightSmall(grid, board.turn());
+  });
+}
+
+for (const cell of board.boardUI.cells) {
+  cell.addEventListener("mouseover", () => {
+    board.boardUI.highlightCell(cell);
+  });
+
+  cell.addEventListener("mouseout", () => {
+    board.boardUI.unhighlightCell(cell);
+  });
+
+  cell.addEventListener("click", () => {
+    if (board.legalMove(cell)) {
+      board.move(cell);
+    } else {
+      board.boardUI.shakeBorder();
+    }
+  });
+}
---
+++ src/js/global.js
@@ -0,0 +1,3 @@
+document.getElementById("title").addEventListener("click", () => {
+  window.location.href = "/";
+})
